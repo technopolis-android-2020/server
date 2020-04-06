5bb7a5b3717f8096592a2c15326cd2715b80f8bd
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2020-04-06 21:37:55 +0300

    Update src/main/java/com/technopolis/server/server/model/User.java
    Co-Authored-By: Alexey Medvedev <medvedev.lasta@yandex.ru>

--- src/main/java/com/technopolis/server/server/model/User.java
+++ src/main/java/com/technopolis/server/server/model/User.java
@@ -12,7 +12,7 @@ public class User extends BaseEntity {
     @Column(name = "username", nullable = false)
     private String username;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @Column(name = "last_name")
