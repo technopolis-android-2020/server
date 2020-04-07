dae85a0efdc7c34df8e34a231565cbb08dfd6c93
MrSandman5 <mrsandman138@gmail.com>
2020-04-07 20:52:01 +0300

    Add resources and work on Updaters

--- src/main/java/com/technopolis/server/server/newsUpdater/impl/RbcNewsUpdater.java
+++ src/main/java/com/technopolis/server/server/newsUpdater/impl/RbcNewsUpdater.java
@@ -1,7 +1,17 @@
 package com.technopolis.server.server.newsUpdater.impl;
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import com.technopolis.server.server.newsUpdater.Updater;
+import lombok.SneakyThrows;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 
 public class RbcNewsUpdater implements Updater {
@@ -11,6 +21,38 @@ public class RbcNewsUpdater implements Updater {
 
     @Override
     public void update() {
+        Connection c;
+        Statement stmt;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/testdb",
+                            "manisha", "123"); //нужна бд для подключения
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            if (lastNewsDate == null){
+                ResultSet rs = stmt.executeQuery( "SELECT max(date) FROM news;" );
+                lastNewsDate = rs.getTimestamp("date").toLocalDateTime();
+                rs.close();
+            }
+
+            SyndFeed rbcNews = new SyndFeedInput().build(new XmlReader(new URL(agentUrl)));
+            for (SyndEntry news : rbcNews.getEntries()) {
+                stmt.executeUpdate("INSERT into news (date, agent, title, body, url) values (" +
+                        news.getPublishedDate() + ", " +
+                        news.getAuthor() + ", " +
+                        news.getTitle() + ", " +
+                        news.getDescription().getValue() + ", " +
+                        news.getUri() + ");");
+            }
+
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
     }
 
     @Override
@@ -18,8 +60,10 @@ public class RbcNewsUpdater implements Updater {
         agentUrl = url;
     }
 
+    @SneakyThrows
     @Override
     public void run() {
         update();
+        Thread.sleep(1000);
     }
 }
--- src/main/java/com/technopolis/server/server/newsUpdater/impl/UpdaterServiceImpl.java
+++ src/main/java/com/technopolis/server/server/newsUpdater/impl/UpdaterServiceImpl.java
@@ -1,24 +1,38 @@
 package com.technopolis.server.server.newsUpdater.impl;
 
+import com.technopolis.server.server.newsUpdater.Updater;
 import com.technopolis.server.server.newsUpdater.UpdaterService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class UpdaterServiceImpl implements UpdaterService {
 
-    private final long timeout = 1000;
-    private List<RbcNewsUpdater> updaters;
-    private ThreadPoolExecutor fixedThreadPoolExecutor;
+    private final String SERVICE_PROP = "service.properties";
+    private final String RSS_LIST = "rssList.txt";
+    private long timeout;
+    private List<RbcNewsUpdater> updaters = new ArrayList<>();
+    private ThreadPoolExecutor executor;
+    private Map<String, String> sources;
 
     @Override
     public void update() {
-        fixedThreadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+        for (Updater updater : updaters) {
+            executor.submit(updater);
+        }
     }
 
     @Override
@@ -28,11 +42,46 @@ public class UpdaterServiceImpl implements UpdaterService {
 
     @Override
     public void init() {
-
+        loadProperties();
+        getSources();
+        for (RbcNewsUpdater updater : updaters) {
+        }
     }
 
+    @SneakyThrows
     @Override
     public void run() {
         update();
+        Thread.sleep(timeout);
+    }
+
+    @SneakyThrows
+    private void loadProperties(){
+        Properties properties = new Properties();
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(SERVICE_PROP);
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + SERVICE_PROP + "' is not found in the classpath");
+        }
+        timeout = Integer.parseInt(properties.getProperty("service.timeout"));
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(Integer.parseInt(properties.getProperty("service.threads")));
+    }
+
+    private void getSources(){
+        sources = new HashMap<>();
+        List<String> list = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(RSS_LIST))) {
+            list = br.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String source : list) {
+            String key = source.substring(0, source.indexOf(":"));
+            String value = source.substring(source.indexOf(":") + 2);
+            sources.putIfAbsent(key, value);
+        }
     }
 }
