839c90fcd80ac4d0335bf2daf644eb8dc9f1b1a3
Alexey <medvedev.lasta@yandex.ru>
2020-04-23 07:03:35 +0700

    Реализовано: fetcher, RBCFetcher, FetcherFactory и FetcherService.

---
+++ src/main/java/com/technopolis/server/server/fetcher/RbcFetcher.java
@@ -0,0 +1,43 @@
+package com.technopolis.server.server.fetcher;
+
+import com.technopolis.server.server.service.impl.AgentServiceImpl;
+import com.technopolis.server.server.service.impl.NewsServiceImpl;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+public class RbcFetcher extends Fetcher {
+
+    RbcFetcher(String rssUrl, AgentServiceImpl agentService, NewsServiceImpl newsService) {
+        super(rssUrl, agentService, newsService);
+        this.agentName = "RBC";
+    }
+
+    @Override
+    String getNewsBody(Document newsDocument) {
+        Document newDocument = Document.createShell("");
+
+        logger.info("RBC_Fetcher: start handling document.");
+        Elements newsParts = newsDocument.body()
+                .getElementsByClass("l-col-main").get(0)
+                .getElementsByClass("article__content");
+
+        for (Element part : newsParts) {
+            logger.info("RBC_Fetcher: start handling element");
+            part.removeClass("article__inline-item");
+            part.removeClass("banner");
+            part.removeClass("r-covid-19");
+            part.removeClass("subscribe-infographic");
+            newDocument.body().appendChild(part);
+        }
+
+        return newDocument.outerHtml();
+    }
+
+    @Override
+    String getNewsTitle(Document newsDocument) {
+        return newsDocument.body().getElementsByClass("article__header__title").first().text();
+    }
+
+}
